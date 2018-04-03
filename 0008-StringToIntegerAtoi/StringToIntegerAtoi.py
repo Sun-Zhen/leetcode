e1e083566770d2d589ac410a4c74a242cb6afb9e
Sun-Zhen <sunzhenhy@gmail.com>
2018-04-03 18:20:54 +0800

    add 8

---
+++ 0008-StringToIntegerAtoi/StringToIntegerAtoi.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+"""
+@author: Alden
+@email: sunzhenhy@gmail.com
+@date: 2018/4/3
+@version: 1.0.0.0
+"""
+
+
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.myAtoi()
