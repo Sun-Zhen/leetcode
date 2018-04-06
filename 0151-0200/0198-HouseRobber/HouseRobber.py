e755d725a86bc394a7b9f960853dcb1c24fd1aed
Sun-Zhen <sunzhenhy@gmail.com>
2018-04-06 17:16:38 +0800

    init

--- 0151-0200/0198-HouseRobber/HouseRobber.py
+++ 0151-0200/0198-HouseRobber/HouseRobber.py
@@ -6,5 +6,23 @@
 @version: 1.0.0.0
 """
 
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        不相邻元素的子数组sum值的最大值
+        """
+        size = len(nums)
+        dp = [0] * (size + 1)
+        if size:
+            dp[1] = nums[0]
+        for i in range(2, size + 1):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
+        return dp[size]
+
+
 if __name__ == "__main__":
-    pass
+    s = Solution()
+    print s.rob([1, 4, 5, 6, 2, 3, 4])
