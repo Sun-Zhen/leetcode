ea4824c870affaf2a4b6f7c948984eaa8c8fa911
Sun-Zhen <sunzhenhy@gmail.com>
2018-04-10 14:58:50 +0800

    update

--- 0101-0150/0104-MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTree.py
+++ 0101-0150/0104-MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTree.py
@@ -6,5 +6,26 @@
 @version: 1.0.0.0
 """
 
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is not None:
+            return max(1 + self.maxDepth(root.right), 1 + self.maxDepth(root.left))
+        else:
+            return 0
+
+
 if __name__ == "__main__":
     pass
