a2619bc8918aacb193b769d4b30bd45e3697cd12
Sun-Zhen <sunzhenhy@gmail.com>
2018-04-10 17:45:53 +0800

    update

--- 0101-0150/0102-BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.py
+++ 0101-0150/0102-BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.py
@@ -6,5 +6,33 @@
 @version: 1.0.0.0
 """
 
-if __name__ == "__main__":
-    pass
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = list()
+        queue = list()
+        if root is not None:
+            queue.append(root)
+            while len(queue) != 0:
+                next_row = list()
+                temp_row = list()
+                for i in range(len(queue)):
+                    temp_row.append(queue[i].val)
+                    if queue[i].left is not None:
+                        next_row.append(queue[i].left)
+                    if queue[i].right is not None:
+                        next_row.append(queue[i].right)
+                res.append(temp_row)
+                queue = next_row
+        return res
--- 0101-0150/0107-BinaryTreeLevelOrderTraversal2/BinaryTreeLevelOrderTraversal2.py
+++ 0101-0150/0107-BinaryTreeLevelOrderTraversal2/BinaryTreeLevelOrderTraversal2.py
@@ -6,5 +6,33 @@
 @version: 1.0.0.0
 """
 
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+# 3
+#  / \
+# 9  20
+#   /  \
+#  15   7
+# [
+#   [15,7],
+#   [9,20],
+#   [3]
+# ]
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        stack = list()
+
+
 if __name__ == "__main__":
     pass
