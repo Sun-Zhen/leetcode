57bf0be9e15f706077d6fd82310f71b2d721a3ef
Sun-Zhen <sunzhenhy@gmail.com>
2018-04-11 08:03:56 +0800

    update

--- 0101-0150/0111-MinimumDepthOfBinaryTree/MinimumDepthOfBinaryTree.py
+++ 0101-0150/0111-MinimumDepthOfBinaryTree/MinimumDepthOfBinaryTree.py
@@ -6,5 +6,47 @@
 @version: 1.0.0.0
 """
 
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        else:
+            res = 0
+            queue = list()
+            queue.append(root)
+            while len(queue) != 0:
+                temp_queue = list()
+                for i in range(len(queue)):
+                    if queue[i].left is None and queue[i].right is None:
+                        return res + 1
+                    if queue[i].left is not None:
+                        temp_queue.append(queue[i].left)
+                    if queue[i].right is not None:
+                        temp_queue.append(queue[i].right)
+                res += 1
+                print res, len(queue)
+                queue = temp_queue
+            return res
+
+
 if __name__ == "__main__":
-    pass
+    a = [1, 2, 3, 4, 5]
+    head = TreeNode(1)
+    head.left = TreeNode(2)
+    head.right = TreeNode(3)
+    head.left.left = TreeNode(4)
+    head.left.right = TreeNode(5)
+    s = Solution()
+    print s.minDepth(head)
--- 0101-0150/0112-PathSum/PathSum.py
+++ 0101-0150/0112-PathSum/PathSum.py
@@ -6,5 +6,40 @@
 @version: 1.0.0.0
 """
 
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        return self.dfs(root, 0, sum)
+
+    def dfs(self, root, sum_val, target):
+        if root is not None:
+            temp_sum = sum_val + root.val
+            left_res = right_res = False
+            if root.left is not None:
+                left_res = self.dfs(root.left, temp_sum, target)
+            if root.right is not None:
+                right_res = self.dfs(root.right, temp_sum, target)
+            if root.right is None and root.left is None and sum_val + root.val == target:
+                return True
+            elif root.right is None and root.left is None and sum_val + root.val != target:
+                return False
+            return left_res or right_res
+        else:
+            return False
+
+
 if __name__ == "__main__":
     pass
