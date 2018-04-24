00f5059ab944689c96f32c6337436a39d511cf2d
Sun-Zhen <sunzhenhy@gmail.com>
2018-04-24 13:55:25 +0800

    update

--- 0001-0050/0015-3Sum/3Sum.py
+++ 0001-0050/0015-3Sum/3Sum.py
@@ -13,8 +13,34 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        res = list()
+        nums = sorted(nums)
+        for i in range(len(nums) - 2):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            num1 = nums[i]
+            t_target = num1 * -1
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                if nums[left] + nums[right] < t_target:
+                    left += 1
+                elif nums[left] + nums[right] > t_target:
+                    right -= 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return res
 
 
 if __name__ == "__main__":
     s = Solution()
-    s.threeSum([-1, 0, 1, 2, -1, -4])
+    print s.threeSum([-1, 0, 1, 2, -1, -4])
+    print s.threeSum([0, 0, 0])
+    print s.threeSum([0, 0, 0, 0])
