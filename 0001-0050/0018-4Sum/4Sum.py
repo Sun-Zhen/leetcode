6fd3646c3466436a5cd7bfecced90993c87fa6fa
Sun-Zhen <sunzhenhy@gmail.com>
2018-04-25 14:01:20 +0800

    update

--- 0001-0050/0018-4Sum/4Sum.py
+++ 0001-0050/0018-4Sum/4Sum.py
@@ -15,8 +15,33 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums = sorted(nums)
+        sum_dict = dict()
+        res = list()
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                sum_dict.setdefault(nums[i] + nums[j], list()).append([i, j])
+        # for k, v in sum_dict.items():
+        #     print k, v
+        for i in range(len(nums) - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums) - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                if target - nums[i] - nums[j] in sum_dict:
+                    tmp_array = sum_dict[target - nums[i] - nums[j]]
+                    first_flag = True
+                    for t_index in range(len(tmp_array)):
+                        if (first_flag and tmp_array[t_index][0] > j) or (tmp_array[t_index][0] > j and nums[tmp_array[t_index][0]] != res[-1][2]):
+                            t = [nums[i], nums[j]]
+                            t.extend([nums[tmp_array[t_index][0]], nums[tmp_array[t_index][1]]])
+                            res.append(t)
+                            first_flag = False
+        return res
 
 
 if __name__ == "__main__":
     s = Solution()
     print s.fourSum([1, 0, -1, 0, -2, 2], 0)
+    print s.fourSum([-3, -2, -1, 0, 0, 1, 2, 3], 0)
+    print s.fourSum([-2, -1, 0, 0, 1, 2], 0)
