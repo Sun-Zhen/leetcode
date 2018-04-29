95c8b8253cb977ddd4a4515ee647a6bba588cf14
Sun-Zhen <sunzhenhy@gmail.com>
2018-04-30 00:06:12 +0800

    0022

--- 0001-0050/0022-GenerateParentheses/GenerateParentheses.py
+++ 0001-0050/0022-GenerateParentheses/GenerateParentheses.py
@@ -5,3 +5,28 @@
 @date: 2018/4/5
 @version: 1.0.0.0
 """
+
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = list()
+
+        def dfs(t_str, i, j):
+            if i == 0 and j == 0:
+                res.append(t_str)
+            if i > 0:
+                dfs(t_str + "(", i - 1, j)
+            if j > i:
+                dfs(t_str + ")", i, j - 1)
+
+        dfs("", n, n)
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.generateParenthesis(3)
