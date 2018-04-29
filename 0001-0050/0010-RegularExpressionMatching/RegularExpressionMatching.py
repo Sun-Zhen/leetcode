cb0517e8cd62f2c3613692fc154ce1f7786ec9f9
Sun-Zhen <sunzhenhy@gmail.com>
2018-04-29 19:05:13 +0800

    回溯法

--- 0001-0050/0010-RegularExpressionMatching/RegularExpressionMatching.py
+++ 0001-0050/0010-RegularExpressionMatching/RegularExpressionMatching.py
@@ -16,23 +16,32 @@ class Solution(object):
         '.' 匹配任意单个字符。
         '*' 匹配零个或多个前面的元素。
         """
-        i = j = 0
-        while i < len(s) and j < len(p):
-            if j == 0:
-                if p[0] == ".":
-                    pass
-                elif p[0] == "*":
-                    pass
-                else:
-                    pass
+        print s, p
+        if len(p) == 0:
+            return len(s) == 0
+        if len(p) == 1:
+            return len(s) == 1 and (p[0] == "." or p[0] == s[0])
+        if p[1] != "*":
+            if len(s) == 0:
+                return False
+            return (s[0] == p[0] or p[0] == ".") and self.isMatch(s[1:], p[1:])
+        while (p[0] == s[0] or p[0] == ".") and len(s) > 0:
+            if self.isMatch(s, p[2:]):
+                return True
+            s = s[1:]
+        return self.isMatch(s, p[2:])
 
 
 if __name__ == "__main__":
     s = Solution()
-    print s.isMatch("aa", "a")  # false
-    print s.isMatch("aa", "aa")  # true
-    print s.isMatch("aaa", "aa")  # false
-    print s.isMatch("aa", "a*")  # true
-    print s.isMatch("aa", ".*")  # true
-    print s.isMatch("ab", ".*")  # true
-    print s.isMatch("aab", "c*a*b")  # true
+    # print s.isMatch("aa", "a")  # false
+    # print s.isMatch("aa", "aa")  # true
+    # print s.isMatch("aaa", "aa")  # false
+    # print s.isMatch("aa", "a*")  # true
+    # print s.isMatch("aa", ".*")  # true
+    # print s.isMatch("ab", ".*")  # true
+    # print s.isMatch("aab", "c*a*b")  # true
+    # print s.isMatch("mississippi", "mis*is*p*.")  # false
+    # print s.isMatch("ab", ".*c")  # false
+    # print s.isMatch("aaa", "ab*ac*a")  # true
+    print s.isMatch("aaca", "ab*a*c*a")  # true
