de1446b5f99797b952dec519d92f4fff53ed66f1
Sun-Zhen <sunzhenhy@gmail.com>
2018-04-30 13:42:59 +0800

    0039+0040

--- 0001-0050/0039-CombinationSum/CombinationSum.py
+++ 0001-0050/0039-CombinationSum/CombinationSum.py
@@ -14,7 +14,23 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        res = list()
+        candidates = sorted(candidates)
+
+        def dfs(t_sum, t_list, index):
+            if t_sum > target:
+                return False
+            elif t_sum == target:
+                res.append(t_list)
+            else:
+                for i in range(index, len(candidates)):
+                    dfs(t_sum + candidates[i], t_list + [candidates[i]], i)
+
+        dfs(0, [], 0)
+        return res
 
 
 if __name__ == "__main__":
-    pass
+    s = Solution()
+    print s.combinationSum([2, 3, 6, 7], 7)
+    print s.combinationSum([2, 3, 5], 8)
--- 0001-0050/0040-CombinationSum2/CombinationSum2.py
+++ 0001-0050/0040-CombinationSum2/CombinationSum2.py
@@ -14,7 +14,25 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        res = list()
+        candidates = sorted(candidates)
+
+        def dfs(t_sum, t_list, index):
+            if t_sum > target:
+                return False
+            elif t_sum == target:
+                res.append(t_list)
+            else:
+                for i in range(index, len(candidates)):
+                    if i > index and candidates[i] == candidates[i - 1]:
+                        continue
+                    dfs(t_sum + candidates[i], t_list + [candidates[i]], i + 1)
+
+        dfs(0, [], 0)
+        return res
 
 
 if __name__ == "__main__":
-    pass
+    s = Solution()
+    print s.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)
+    # print s.combinationSum2([2, 5, 2, 1, 2], 5)
